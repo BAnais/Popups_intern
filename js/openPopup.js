7c928a06f9094b3ba5e51461e573571f19f016f8
Unknown <anais.banos@gmail.com>
2018-02-16 10:19:14 +0100

    Init repo

---
+++ js/openPopup.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event){
+
+  document.getElementById('subSend').onclick= function openSend() {
+    alert("Expédition");
+  };
+  document.getElementById('subReceive').onclick= function openSend() {
+    alert("Retour");
+  };
+  document.getElementById('subSav').onclick= function openSend() {
+    alert("SAV");
+  };
+});
