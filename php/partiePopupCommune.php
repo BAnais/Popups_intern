3a355919edaa318f5b067e815bea44cebb584d1c
BAnais <anais.banos@gmail.com>
2018-02-16 12:07:02 +0100

    add container + div label transporteur

--- php/partiePopupCommune.php
+++ php/partiePopupCommune.php
@@ -9,7 +9,12 @@
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+  <div class="container">
+
   <div class="row">
+    <div class="col-sm-4">
+      <label>Transporteur : </label>
+    </div>
     <div class="col-sm-8">
 
       <input  type="radio" name="transporteur" value="UPS" id="transporteurChoice1">
@@ -19,6 +24,11 @@
 
     </div>
   </div>
+    <div class="col-sm-8" id="transporteurConditionYes">
+
+
+    </div>
+</div>
 </body>
 </html>
 <?php
