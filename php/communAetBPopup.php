d7f46fc8dca6f57c40baf8925baf51c3dcf019bd
BAnais <anais.banos@gmail.com>
2018-02-16 16:03:45 +0100

    add div radio deploiement

--- php/communAetBPopup.php
+++ php/communAetBPopup.php
@@ -16,12 +16,13 @@
         <label>Envoi Groupé : </label>
       </div>
       <div class="col-lg-8">
-      <input type="radio" name="yes" value="yes" id="EnvGroupYes">
+      <input type="radio" name="groupe" value="yes" id="EnvGroupYes">
       <label for="EnvGroupYes">Oui</label>
-      <input type="radio" name="no" value="no" id="EnvGroupNo">
+      <input type="radio" name="groupe" value="no" id="EnvGroupNo">
       <label for="EnvGroupNo">Non</label>
       </div>
     </div>
+
     <div class="row">
       <div class="col-lg-4">
         <label>Nombre de colis :</label>
@@ -29,6 +30,18 @@
       <div class="col-lg-8">
         <input type="number" name="nbColis" placeholder="XXX" min="2" max="10">
       </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-4">
+          <label>Déploiement : </label>
+        </div>
+        <div class="col-lg-8">
+          <input type="radio" name="deploiement" value="Yes" id="deploiementYes">
+          <label for="deploiementYes">Oui</label>
+          <input type="radio" name="deploiement" value="No" id="deploiementNo">
+          <label for="deploiementNo">Non</label>
+        </div>
     </div>
   </div>
 </body>
