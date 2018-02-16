4c6d7b048d8e4e16bbb708940876a9c63c189ca5
BAnais <anais.banos@gmail.com>
2018-02-16 12:02:55 +0100

    add bootstrap & jquery + div row et div col-sm-8

--- php/partiePopupCommune.php
+++ php/partiePopupCommune.php
@@ -1,9 +1,26 @@
 <html>
-  <input  type="radio" name="transporteur" value="UPS" id="transporteurChoice1">
-  <label for="transporteurChoice1">UPS</label>
-  <input type="radio" name="transporteur2" value="DHL_Express" id="transporteurChoice2">
-  <label for="transporteurChoice2">DHL Express</label>
+<head>
+  <meta charset="utf-8">
+  <script src="../js/...js" charset="utf-8"></script>
+  <script
+  src="http://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+  crossorigin="anonymous"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+  <div class="row">
+    <div class="col-sm-8">
+
+      <input  type="radio" name="transporteur" value="UPS" id="transporteurChoice1">
+      <label for="transporteurChoice1">UPS</label>
+      <input type="radio" name="transporteur2" value="DHL_Express" id="transporteurChoice2">
+      <label for="transporteurChoice2">DHL Express</label>
+
+    </div>
+  </div>
+</body>
 </html>
 <?php
 
- ?>
+?>
