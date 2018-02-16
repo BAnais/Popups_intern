9bf4ed204e746e83722c1933de40581862a01a0b
BAnais <anais.banos@gmail.com>
2018-02-16 11:55:03 +0100

    add radio buttuon Ups & DHL

--- php/partiePopupCommune.php
+++ php/partiePopupCommune.php
@@ -1,3 +1,9 @@
+<html>
+  <input  type="radio" name="transporteur" value="UPS" id="transporteurChoice1">
+  <label for="transporteurChoice1">UPS</label>
+  <input type="radio" name="transporteur2" value="DHL_Express" id="transporteurChoice2">
+  <label for="transporteurChoice2">DHL Express</label>
+</html>
 <?php
 
  ?>
