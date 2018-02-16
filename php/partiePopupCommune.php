c4362a99eff4d1df5c3ab0b1ffda73208a1cca38
BAnais <anais.banos@gmail.com>
2018-02-16 15:41:31 +0100

    change col-sm col-lg

--- php/partiePopupCommune.php
+++ php/partiePopupCommune.php
@@ -12,10 +12,10 @@
   <div class="container">
 
     <div class="row">
-      <div class="col-sm-4">
+      <div class="col-lg-4">
         <label>Transporteur : </label>
       </div>
-      <div class="col-sm-8">
+      <div class="col-lg-8">
         <input  type="radio" name="transporteur" value="Transporteur1" id="transporteurChoice1">
         <label for="transporteurChoice1">Transporteur1</label>
         <input type="radio" name="transporteur" value="Transporteur2" id="transporteurChoice2">
@@ -24,10 +24,10 @@
     </div>
 
     <div class="row" id="transporteurConditionYes">
-      <div class="col-sm-4">
+      <div class="col-lg-4">
         <label>Bon T1 : </label>
       </div>
-      <div class="col-sm-8">
+      <div class="col-lg-8">
         <input type="radio" name="bonT1" value="Oui" id="bonT1Choice1">
         <label for="bonUPSChoice1">Oui</label>
         <input type="radio" name="bonT2" value="Non" id="bonT2Choice2">
@@ -36,43 +36,43 @@
     </div>
 
     <div class="row" >
-      <div class="col-sm-4">
+      <div class="col-lg-4">
         <label>N° Suivi</label>
       </div>
-      <div class="col-sm-8">
+      <div class="col-lg-8">
         <input type="text" name="numSuivi" placeholder="123456789">
       </div>
     </div>
 
     <div class="row">
-      <div class="col-sm-4">
+      <div class="col-lg-4">
         <label>Prix Transporteur</label>
       </div>
-      <div class="col-sm-8">
+      <div class="col-lg-8">
         <input type="text" name="prixTransporteur" placeholder="XXX €">
       </div>
     </div>
 
     <div class="row">
-      <div class="col-sm-4">
+      <div class="col-lg-4">
         <label>Date d'envoi</label>
       </div>
-      <div class="col-sm-8">
+      <div class="col-lg-8">
         <input type="date" name="dateEnvoi" value="">
       </div>
     </div>
 
     <div class="row">
-      <div class="col-sm-3">
+      <div class="col-lg-3">
         <label>Date livraison</label>
       </div>
-      <div class="col-sm-3">
+      <div class="col-lg-3">
         <input type="date" name="dateLivraison">
       </div>
-      <div class="col-sm-2">
+      <div class="col-lg-2">
         <label>Horaire (24h format)</label>
       </div>
-      <div class="col-sm-4">
+      <div class="col-lg-4">
 					<select name="heure">
 						<option value="h">heure</option>
 						<?php for ($heure = 00 ; $heure <= 23 ; $heure++):
@@ -93,19 +93,19 @@
       </div>
 
       <div class="row">
-        <div class="col-sm-4">
+        <div class="col-lg-4">
           <label>Raison d'envoi</label>
         </div>
-        <div class="col-sm-8">
+        <div class="col-lg-8">
           <textarea name="raisonEnvoi" rows="10" cols="80" readonly="false" placeholder="Entrez la raison de l'envoi ici" ></textarea>
         </div>
       </div>
 
       <div class="row">
-        <div class="col-sm-4">
+        <div class="col-lg-4">
           <label>Langue</label>
         </div>
-        <div class="col-sm-8">
+        <div class="col-lg-8">
           <input type="radio" name="FR" value="FR" id="FR1">
           <label for="FR1">FR</label>
           <input type="radio" name="EN" value="EN" id="EN1">
@@ -113,10 +113,10 @@
         </div>
 
         <div class="row">
-          <div class="col-sm-4">
+          <div class="col-lg-4">
             <label>Mail CC</label>
           </div>
-          <div class="col-sm-8">
+          <div class="col-lg-8">
             <input type="email" name="emailCC" placeholder="exemple@entreprise.fr">
           </div>
         </div>
