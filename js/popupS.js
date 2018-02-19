a88713c4f7883d66af9476f4009737dec8519233
BAnais <anais.banos@gmail.com>
2018-02-19 14:00:04 +0100

    window.open("../php/popupTemplate.php");

--- js/popupS.js
+++ js/popupS.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
-  document.getElementById('subSend').onclick= function openSend() {
-    alert("Expédition");
+  document.getElementsByTagName('Send').onclick= function openSend() {
+    window.open("../php/popupTemplate.php");
   };
-  
+
 
   $(document).ready(function(){
     //document.title="Expédition"
