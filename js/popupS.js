71999875117f40de8ba599aacc8d03ffd33b3b92
BAnais <anais.banos@gmail.com>
2018-02-19 15:09:45 +0100

    function openPopup(){   window.open('about:blank', 'Popup', 'width=200,height=700,scollbars=1,resizable=0'); });

--- js/popupS.js
+++ js/popupS.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
 function openPopup(){
-  window.open("window.open('about:blank', 'Popup', 'width=200,height=700,scollbars=1,resizable=0');")
-}
+  window.open('about:blank', 'Popup', 'width=200,height=700,scollbars=1,resizable=0');
+});
+
 });
