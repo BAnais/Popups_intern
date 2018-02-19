bad90de95382b5aeebcce02dc2923818b21ead3c
BAnais <anais.banos@gmail.com>
2018-02-19 14:22:07 +0100

    test popup remove function on click

--- js/popupS.js
+++ js/popupS.js
@@ -1,12 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event){
-
-  document.getElementById('Send').onclick= function openSend() {
-    popup = window.open("../php/popupTemplate.php", 'Popup','height=200, width=150');
-  };
-
-
   $(document).ready(function(){
     //document.title="Expédition"
-
+    window.open("../php/popupTemplate.php", 'Popup','height=200, width=150');
   });
 });
