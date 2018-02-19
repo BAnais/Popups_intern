4ea54797995d6d3d8aebbd4dd9e2d371eb17f92c
BAnais <anais.banos@gmail.com>
2018-02-19 14:52:14 +0100

    //}); popupS.js

--- js/popupS.js
+++ js/popupS.js
@@ -3,5 +3,5 @@ document.addEventListener("DOMContentLoaded", function(event){
 //  window.onload(function(){
     //document.title="Expédition"
 //    window.open("../php/popupTemplate.php", 'Popup','height=200, width=150');
-  });
+//  });
 });
