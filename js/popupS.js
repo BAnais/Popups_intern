26d5b7367b6fc06a4b9002654c4e77c9eec7a42a
BAnais <anais.banos@gmail.com>
2018-02-19 14:18:43 +0100

    Test open popup

--- js/popupS.js
+++ js/popupS.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
   document.getElementById('Send').onclick= function openSend() {
-    window.open("../php/popupTemplate.php");
+    popup = window.open("../php/popupTemplate.php", 'Popup','height=200, width=150');
   };
 
 
