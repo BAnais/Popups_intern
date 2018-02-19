f7c75295a1f469143d75e56e48cb76b9caf8d3bc
BAnais <anais.banos@gmail.com>
2018-02-19 12:10:53 +0100

    form post index

--- js/popupS.js
+++ js/popupS.js
@@ -3,12 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event){
   document.getElementById('subSend').onclick= function openSend() {
     alert("Expédition");
   };
-  document.getElementById('subReturn').onclick= function openSend() {
-    alert("Retour");
-  };
-  document.getElementById('subSav').onclick= function openSend() {
-    alert("SAV");
-  };
+  
 
   $(document).ready(function(){
     //document.title="Expédition"
