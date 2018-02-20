d1ea6111f39d065f80e7b5ef3569ffa7c379d0ab
BAnais <anais.banos@gmail.com>
2018-02-20 09:57:07 +0100

    <div class="col-sm-3"> <div class="col-sm-6">

--- includes/php/Expedition.php
+++ includes/php/Expedition.php
@@ -2,10 +2,10 @@
 include ('../includes/html/sub_popup_Exp_Ret.html');
  ?>
 <div class="row">
-    <div class="col-lg-2">
+    <div class="col-sm-3">
       <label>Configuré : </label>
     </div>
-    <div class="col-lg-8">
+    <div class="col-sm-6">
       <input type="radio" name="Configuration" value="Yes" id="ConfigureYes">
       <label for="ConfigureYes">Oui</label>
       <input type="radio" name="Configuration" value="No" id="ConfigureNo">
@@ -13,10 +13,10 @@ include ('../includes/html/sub_popup_Exp_Ret.html');
     </div>
   </div>
 <div class="row">
-  <div class="col-lg-2">
+  <div class="col-sm-3">
     <label>Prix Matériel : </label>
   </div>
-  <div class="col-lg-8">
+  <div class="col-sm-6">
     <input type="text" name="prix" placeholder=" affichage bdd">
   </div>
 </div>
