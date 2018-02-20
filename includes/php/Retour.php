409c7d1a32c544ceb5489522817b28cd8ce7775f
BAnais <anais.banos@gmail.com>
2018-02-20 11:58:21 +0100

    col-lg to col-sm

--- includes/php/Retour.php
+++ includes/php/Retour.php
@@ -3,10 +3,10 @@ include ('../includes/html/sub_popup_Exp_Ret.html');
  ?>
 
 <div class="row">
-  <div class="col-lg-2">
+  <div class="col-sm-2">
     <label>Matériel Connu : </label>
   </div>
-  <div class="col-lg-8">
+  <div class="col-sm-6">
     <input type="radio" name="Connu" value="Yes" id="ConnuYes">
     <label for="ConnuYes">Oui</label>
     <input type="radio" name="Connu" id="ConnuNo">
@@ -14,10 +14,10 @@ include ('../includes/html/sub_popup_Exp_Ret.html');
   </div>
 </div>
 <div class="row" id="numSerieRow">
-  <div class="col-lg-2">
+  <div class="col-sm-2">
     <label>Numéro de série : </label>
   </div>
-  <div class="col-lg-8">
+  <div class="col-sm-6">
     <input type="text" name="numSerie" placeholder="affichage bdd"> <!-- si inconnu dans bdd alors afficher "inconnu"-->
   </div>
 </div>
