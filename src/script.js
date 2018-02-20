f440660f32f0ec87a7c9f589ee0c56a07e0f561a
BAnais <anais.banos@gmail.com>
2018-02-20 10:32:44 +0100

    test jquery

--- src/script.js
+++ src/script.js
@@ -1,16 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(event){
 /*
+document.addEventListener("DOMContentLoaded", function(event){
 function openPopup(){
   document.window.open('', '_blank', 'width=200,height=700,scollbars=1,resizable=0');
 };
-*/
 //doc pret
 $(document).ready(function(){
 
   //Dynamique expedition.php
-  document.getElementById('pMatos').style.visibility = "hidden";
+//  document.getElementById('pMatos').style.visibility = "hidden";
 console.log("Document chargé");
 
 });
 
+
 });
+*/
+window.onload = function() {
+    if (window.jQuery) {
+        alert("Jquery OK");
+    } }
