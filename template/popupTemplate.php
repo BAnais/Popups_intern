3f2632e29074581f6f3b53f9ebe21dcc93549620
BAnais <anais.banos@gmail.com>
2018-02-20 11:47:55 +0100

    <title><?php echo $typeCategorie ;?></title>

--- template/popupTemplate.php
+++ template/popupTemplate.php
@@ -1,3 +1,6 @@
+<?php
+    $typeCategorie=$_POST['type'];
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,12 +8,12 @@
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
   <script src="../src/script.js" charset="utf-8"></script>
-  <title>Popup</title>
+  <title><?php echo $typeCategorie ;?></title>
 </head>
 <body>
   <div class="container">
     <?php
-    $typeCategorie=$_POST['type'];
+
     include ('../includes/php/'.$typeCategorie.'.php');
      ?>
   </div>
