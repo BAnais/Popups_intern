89f948e0e5bf18a799b76dbb364b925bff8023f4
BAnais <anais.banos@gmail.com>
2018-02-20 10:46:44 +0100

    link jquery

--- template/popupTemplate.php
+++ template/popupTemplate.php
@@ -2,9 +2,9 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <script src="../src/script.js" charset="utf-8"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script src="../src/script.js" charset="utf-8"></script>
   <title>Popup</title>
 </head>
 <body>
