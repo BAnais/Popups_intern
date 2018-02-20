542e58e903023d8f60ca9afc2767f741fef1daf6
BAnais <anais.banos@gmail.com>
2018-02-20 10:19:53 +0100

    $(document).getElementById('pMatos').style.display = "hidden";

--- src/script.js
+++ src/script.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event){
-
+/*
 function openPopup(){
-  window.open('', '_blank', 'width=200,height=700,scollbars=1,resizable=0');
+  document.window.open('', '_blank', 'width=200,height=700,scollbars=1,resizable=0');
 };
+*/
+//doc pret
+$(document).ready(function(){
+
+  //Dynamique expedition.php
+  $(document).getElementById('pMatos').style.display = "hidden";
+
+
+});
 
 });
