0f069a7e6ef316f663e71987633aa75c852af2d6
BAnais <anais.banos@gmail.com>
2018-02-20 10:51:09 +0100

    ../src/scrpit.js

--- template/popupTemplate.php
+++ template/popupTemplate.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script src="./src/script.js" charset="utf-8"></script>
+  <script src="../src/script.js" charset="utf-8"></script>
   <title>Popup</title>
 </head>
 <body>
