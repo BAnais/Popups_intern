76097809a609c195037698bb440ed19fb0c4fe25
BAnais <anais.banos@gmail.com>
2018-02-21 11:38:14 +0100

    console.log($(this));

--- src/script.js
+++ src/script.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
 $('input[type="radio"]').on('click',function(){
 
 $('#'+$('input[type="radio"].prop("checked")').data('divId')).css('display',$('input[type="radio"].prop("checked")').data('display'));
-console.log($('input[type="radio"].prop("checked")'));
+console.log($(this));
 /*
 if($('#EnvGroupYes').prop('checked')){
 	$('#nbColisRow').css('display',"block");
