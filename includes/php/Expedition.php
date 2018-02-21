ce3781eb514e1da93b18dcc05fca3da0a2193444
BAnais <anais.banos@gmail.com>
2018-02-21 13:45:30 +0100

    data-divname="contentpMatos"

--- includes/php/Expedition.php
+++ includes/php/Expedition.php
@@ -6,18 +6,18 @@ include ('../includes/html/sub_popup_Exp_Ret.html');
       <label>Configuré : </label>
     </div>
     <div class="col-sm-6">
-      <input type="radio" name="Configuration" value="Yes" id="ConfigureYes" data-display="inline-block" data-divId="pMatos">
+        <input type="radio" name="Configuration" value="Yes" id="ConfigureYes" data-display="block" data-divId="pMatos" data-divname="contentpMatos">
       <label for="ConfigureYes">Oui</label>
-      <input type="radio" name="Configuration" value="No" id="ConfigureNo" data-display="none"  data-divId="pMatos">
+      <input type="radio" name="Configuration" value="No" id="ConfigureNo" data-display="none"  data-divId="pMatos" data-divname="contentpMatos">
       <label for="ConfigureNo">Non</label>
     </div>
   </div>
 <div class="row" id="pMatos">
-  <div class="col-sm-3">
+  <div class="col-sm-3" name="contentpMatos">
     <label>Prix Matériel : </label>
   </div>
   <div class="col-sm-6">
-    <input type="text" name="prix" placeholder=" affichage bdd">
+    <input type="text" name="contentpMatos" placeholder=" affichage bdd">
   </div>
 </div>
 <?php
