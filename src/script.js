19818a76456749eb960a4f3811f3201fee532605
BAnais <anais.banos@viacesi.fr>
2018-02-28 14:12:09 +0100

    script.js : remove if else concernant le contentEnvGroup -> remplacé dans la partie dynamique

--- src/script.js
+++ src/script.js
@@ -18,122 +18,112 @@ $(document).ready(function(){
     selectOtherMonths: true,
     dateFormat: "dd-mm-yy"
   });
-    //ouverture modal
-    $("#myBtn").on('click',function(){
-      $("#myModal").modal();
-    });
+  //ouverture modal
+  $("#myBtn").on('click',function(){
+    $("#myModal").modal();
   });
+});
 
-  $('[name=dateEnvoi]').on('change', function(){
+$('[name=dateEnvoi]').on('change', function(){
 
-    $('[name=dateLivraison]').removeAttr('disabled');
-    var dateSelected = new Date($.datepicker.parseDate("dd-mm-yy", $(this).val()));
-    dateSelected.setDate(dateSelected.getDate() + 2);
+  $('[name=dateLivraison]').removeAttr('disabled');
+  var dateSelected = new Date($.datepicker.parseDate("dd-mm-yy", $(this).val()));
+  dateSelected.setDate(dateSelected.getDate() + 2);
 
-    $('[name=dateLivraison]').datepicker("option", "minDate", dateSelected);
+  $('[name=dateLivraison]').datepicker("option", "minDate", dateSelected);
 
-  });
+});
 
-  $('#myModal').on('show.bs.modal',function(){
-    console.log($('#titleCategorie').text());
-    //sub_popup
-    $('#transporteurConditionYesRow').css('display','none');
+$('#myModal').on('show.bs.modal',function(){
+  console.log($('#titleCategorie').text());
+  //sub_popup
+  $('#transporteurConditionYesRow').css('display','none');
 
-    switch (document.title) {
-      case "Expedition":
-      // expedition.php
-      $('#pMatos').css('display','none');
-      //sub_popup_Exp_Ret
-      $('#nbColisRow').css('display','none');
-      $('#numDevisRow').css('display','none');
-      break;
+  switch (document.title) {
+    case "Expedition":
+    // expedition.php
+    $('#pMatos').css('display','none');
+    //sub_popup_Exp_Ret
+    $('#nbColisRow').css('display','none');
+    $('#numDevisRow').css('display','none');
+    break;
 
-      case "Retour":
-      //Retour
-      $('#numSerieRow').css('display','none');
-      //sub_popup_Exp_Ret
-      $('#nbColisRow').css('display','none');
-      $('#numDevisRow').css('display','none');
-      break;
+    case "Retour":
+    //Retour
+    $('#numSerieRow').css('display','none');
+    //sub_popup_Exp_Ret
+    $('#nbColisRow').css('display','none');
+    $('#numDevisRow').css('display','none');
+    break;
 
-      case "SAV":
-      //sav
-      $('#savExpYesRow').css('display','none');
-      break;
+    case "SAV":
+    //sav
+    $('#savExpYesRow').css('display','none');
+    break;
 
-      default:
-      break;
+    default:
+    break;
+  }
+
+});
+
+//detecte un clic bouton radio et réagis dynamiquement en fonction de ce dernier
+$('input[type="radio"]').on('click',function(){
+  //récupère l'id de la div row correspondante dans data-divId puis change le style display
+  //selon l'instruction displaydivrow
+  $('#'+$(this).attr('data-divId')).css('display',$(this).data('display'));
+  //si data('display')="block" alors on passe divname concernées required à true
+  //sinon si = none alors on passe required à false
+  if($(this).data('display') == "block"){
+    if($(this).prop('id')=='ConnuYes'){
+      $('[name=numSerie]').val('');
+    }
+    $('[name='+$(this).data('divname')+']').attr('required','true');
+
+  }else if($(this).data('display') == "none"){
+
+    $('[name='+$(this).data('divname')+']').removeAttr('required');
+    $('[name='+$(this).data('divname')+']').val('');
+
+    if($(this).prop('id')=='ConnuNo'){
+      $('[name=numSerie]').val('inconnu');
     }
 
-  });
+  }
+  //récupère le name des divs correspondantes dans data
+  //  $('[name='+$(this).data('divname')+']').css('display','inline-'+$(this).data('display'));
 
-  //detecte un clic bouton radio et réagis dynamiquement en fonction de ce dernier
-  $('input[type="radio"]').on('click',function(){
-    //récupère l'id de la div row correspondante dans data-divId puis change le style display
-    //selon l'instruction displaydivrow
-    $('#'+$(this).attr('data-divId')).css('display',$(this).data('display'));
-    //si data('display')="block" alors on passe divname concernées required à true
-    //sinon si = none alors on passe required à false
-    if($(this).data('display') == "block"){
-      if($(this).data('divname') == 'contentEnvGroup'){
-        $('[name=NbColis]').attr('required','true');
-      //  $('[name=prixTransporteur]').attr('required','true');
-      }else{
-        if($(this).prop('id')=='ConnuYes'){
-          $('[name=numSerie]').val('');
-        }
-        $('[name='+$(this).data('divname')+']').attr('required','true');
-      }
-    }else if($(this).data('display') == "none"){
-      if($(this).data('divname') == 'contentEnvGroup'){
-        $('[name=NbColis]').removeAttr('required');
-        $('[name=NbColis]').val('');
-    //    $('[name=prixTransporteur]').removeAttr('required');
-    //    $('[name=prixTransporteur]').val('');
-      }else{
+  //quand un click sur btn radio transporteur on récupère sa value pour l'ajouter
+  // au nom du bon (ligne suivante) et uncheck les radio bonTr au clic du Tr
+  if($(this).attr('name')=='transporteur' && $(this).prop("checked") ){
+    console.log('changement valeur label');
+    $('#bonTr').html("Bon "+$(this).attr('value')+ ":");
+    $('[name="contentTrCondYes"]').prop('checked', false);
+  }
+  //popupTemplate changement de la valeur du bouton en fct de la langue selectionnée
+  if($(this).attr('name')=='langue' && $(this).prop("checked")){
+    $('[name=btnSend]').val($(this).data('btn'));
+  }
+  /*
+  //test console log
+  console.log($(this).attr('data-divId'));
+  console.log($(this).data('divname'));
+  console.log($(this).data('display'));
+  console.log($(this).val());
+  */
 
-        $('[name='+$(this).data('divname')+']').removeAttr('required');
-        $('[name='+$(this).data('divname')+']').val('');
+});
+//au changement de l'input date d'envoi on modifie le tag min de date de livraison min a +2
+$('[name=dateEnvoi]').on('change', function(){
+  console.log("test date"+$('[name=dateEnvoi]').val());
+  var recupDate = $('[name=dateEnvoi]').val();
+  var date = recupDate.split("-");
+  var year = date[0];
+  var month = date[1];
+  var jour = date[2]
+  var jourReceive = jour +2
+  $('[name=dateLivraison]').min= jourReceive+"-"+month+"-"+year;
 
-        if($(this).prop('id')=='ConnuNo'){
-          $('[name=numSerie]').val('inconnu');
-        }
-      }
-    }
-    //récupère le name des divs correspondantes dans data
-    //  $('[name='+$(this).data('divname')+']').css('display','inline-'+$(this).data('display'));
-
-    //quand un click sur btn radio transporteur on récupère sa value pour l'ajouter
-    // au nom du bon (ligne suivante) et uncheck les radio bonTr au clic du Tr
-    if($(this).attr('name')=='transporteur' && $(this).prop("checked") ){
-      console.log('changement valeur label');
-      $('#bonTr').html("Bon "+$(this).attr('value')+ ":");
-      $('[name="contentTrCondYes"]').prop('checked', false);
-    }
-    //popupTemplate changement de la valeur du bouton en fct de la langue selectionnée
-    if($(this).attr('name')=='langue' && $(this).prop("checked")){
-      $('[name=btnSend]').val($(this).data('btn'));
-    }
-    /*
-    //test console log
-    console.log($(this).attr('data-divId'));
-    console.log($(this).data('divname'));
-    console.log($(this).data('display'));
-    console.log($(this).val());
-    */
-
-  });
-  //au changement de l'input date d'envoi on modifie le tag min de date de livraison min a +2
-  $('[name=dateEnvoi]').on('change', function(){
-    console.log("test date"+$('[name=dateEnvoi]').val());
-    var recupDate = $('[name=dateEnvoi]').val();
-    var date = recupDate.split("-");
-    var year = date[0];
-    var month = date[1];
-    var jour = date[2]
-    var jourReceive = jour +2
-    $('[name=dateLivraison]').min= jourReceive+"-"+month+"-"+year;
-
-  });
+});
 
 });
