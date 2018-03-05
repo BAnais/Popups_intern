a3d825e1914c1bd0f4b481b3112c7dc8ea26028e
BAnais <anais.banos@viacesi.fr>
2018-03-05 16:01:25 +0100

    add class isRequired pour SAV sans expédition remove attr required du form

--- includes/php/sub_popup.php
+++ includes/php/sub_popup.php
@@ -7,7 +7,7 @@
     <input type="text" readonly name="VE" value="VE">
   </div>
   <div class="col-xs-5">
-    <input type="text" name="assoVe" required >
+    <input class="isRequired" type="text" name="assoVe" required >
   </div>
 </div>
 
@@ -16,9 +16,9 @@
     <label>Transporteur : </label>
   </div>
   <div class="col-sm-6 ">
-    <input  type="radio" name="transporteur" value="Transporteur1" id="transporteurChoice1" data-display="block" data-divId="transporteurConditionYesRow" data-divname="contentTrCondYes" required>
+    <input class="isRequired" type="radio" name="transporteur" value="Transporteur1" id="transporteurChoice1" data-display="block" data-divId="transporteurConditionYesRow" data-divname="contentTrCondYes" required>
     <label for="transporteurChoice1">Transporteur1</label>
-    <input type="radio" name="transporteur" value="Transporteur3" id="transporteurChoice3" data-display="block" data-divId="transporteurConditionYesRow"  data-divname="contentTrCondYes" required >
+    <input class="isRequired" type="radio" name="transporteur" value="Transporteur3" id="transporteurChoice3" data-display="block" data-divId="transporteurConditionYesRow"  data-divname="contentTrCondYes" required >
     <label for="transporteurChoice3">Transporteur3</label>
   </div>
 </div>
@@ -28,9 +28,9 @@
     <label id="bonTr">Bon Transporteur : </label>
   </div>
   <div class="col-sm-6" >
-    <input type="radio" name="contentTrCondYes" value="yes" id="bonOui" required>
+    <input class="isRequired" type="radio" name="contentTrCondYes" value="yes" id="bonOui" required>
     <label for="bonChoice1">Oui</label>
-    <input type="radio" name="contentTrCondYes" value="no" id="bonNon" required>
+    <input class="isRequired" type="radio" name="contentTrCondYes" value="no" id="bonNon" required>
     <label for="bonChoice2">Non</label>
   </div>
 </div>
@@ -40,7 +40,7 @@
     <label>Prix Transporteur</label>
   </div>
   <div class="col-sm-6 " >
-    <input type="number" name="prixTransporteur" placeholder="XXX €">
+    <input class="isRequired" type="number" name="prixTransporteur" placeholder="XXX €" required >
   </div>
 </div>
 
@@ -50,7 +50,7 @@
     <label>N° Suivi</label>
   </div>
   <div class="col-sm-6 ">
-    <input type="text" name="numSuivi" placeholder="123456769" required>
+    <input class="isRequired" type="text" name="numSuivi" placeholder="123456769" required>
   </div>
 </div>
 
@@ -59,7 +59,7 @@
     <label>Date d'envoi</label>
   </div>
   <div class="col-sm-6 ">
-    <input type="text" name="dateEnvoi" required>
+    <input class="isRequired" type="text" name="dateEnvoi" required>
   </div>
 </div>
 
@@ -68,7 +68,7 @@
     <label>Date livraison</label>
   </div>
   <div class="col-sm-6 ">
-    <input type="text" name="dateLivraison" disabled required>
+    <input class="isRequired" type="text" name="dateLivraison" disabled required>
   </div>
 </div>
 <div class="row">
@@ -77,7 +77,7 @@
   </div>
   <div class="col-sm-6   ">
     <label>  Entre :  </label>
-    <select name="Dheure" required>
+    <select class="isRequired" name="Dheure" required>
       <option disabled selected value="">Heure</option>
       <?php for ($Dheure = 00 ; $Dheure <= 23 ; $Dheure++):
         $Dhour = sprintf("%02d", $Dheure);
@@ -86,7 +86,7 @@
       <?php endfor; ?>
     </select>
     <label>  :  </label>
-    <select name="Dminutes" required disabled>
+    <select class="isRequired" name="Dminutes" required disabled>
       <option disabled selected value="">Minutes</option>
       <?php for ($Dminutes = 00 ; $Dminutes <= 59 ; $Dminutes++):
         $Dmin = sprintf("%02d", $Dminutes);
@@ -95,7 +95,7 @@
       <?php endfor; ?>
     </select>
     <label>  Et : </label>
-    <select name="Fheure" required disabled>
+    <select  class="isRequired" name="Fheure" required disabled>
       <option disabled selected value="">Heure</option>
       <?php for ($Fheure = 00 ; $Fheure <= 23 ; $Fheure++):
         $Fhour = sprintf("%02d", $Fheure);
@@ -104,7 +104,7 @@
       <?php endfor; ?>
     </select>
     <label>  :  </label>
-    <select name="Fminutes" required disabled>
+    <select class="isRequired" name="Fminutes" required disabled>
       <option disabled selected value="">Minutes</option>
       <?php for ($Fminutes = 00 ; $Fminutes <= 59 ; $Fminutes++):
         $Fmin = sprintf("%02d", $Fminutes);
@@ -120,7 +120,7 @@
     <label>Raison d'envoi</label>
   </div>
   <div class="col-sm-6 ">
-    <textarea style="resize: none; overflow: vertical;" name="raisonEnvoi" rows="5" cols="30" editable="true" placeholder="Entrez la raison de l'envoi ici" required></textarea>
+    <textarea class="isRequired" style="resize: none; overflow: vertical;" name="raisonEnvoi" rows="5" cols="30" editable="true" placeholder="Entrez la raison de l'envoi ici" required></textarea>
   </div>
 </div>
 
@@ -129,9 +129,9 @@
     <label>Langue</label>
   </div>
   <div class="col-sm-6 ">
-    <input type="radio" name="langue" value="FR" id="FR1" data-btn="Envoyer" required>
+    <input class="isRequired" type="radio" name="langue" value="FR" id="FR1" data-btn="Envoyer" required>
     <label for="FR1">FR</label>
-    <input type="radio"  name="langue" value="EN" id="EN1" data-btn="Send" required>
+    <input class="isRequired" type="radio"  name="langue" value="EN" id="EN1" data-btn="Send" required>
     <label for="EN1">EN</label>
   </div>
 </div>
