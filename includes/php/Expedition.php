137cb428ffb1441f46cab05c828789d39df69e3b
BAnais <anais.banos@viacesi.fr>
2018-03-12 09:03:27 +0100

    expedition.php : comment div prix matos

--- includes/php/Expedition.php
+++ includes/php/Expedition.php
@@ -1,26 +1,29 @@
 <?php
 include ('../includes/html/sub_popup_Exp_Ret.html');
- ?>
+?>
 <div class="row">
-    <div class="col-sm-3 ">
-      <label>Configuré : </label>
-    </div>
-    <div class="col-sm-6 ">
-        <input type="radio" name="Configuration" value="yes" id="ConfigureYes" data-display="block" data-divId="pMatos" required>
-      <label for="ConfigureYes">Oui</label>
-      <input type="radio" name="Configuration" value="no" id="ConfigureNo" data-display="none"  data-divId="pMatos" required>
-      <label for="ConfigureNo">Non</label>
-    </div>
+  <div class="col-sm-3 ">
+    <label>Configuré : </label>
   </div>
-<div class="row" id="pMatos">
-  <div class="col-sm-3" >
-    <label>Prix Matériel : </label>
-  </div>
-  <div class="col-sm-6">
-    <input type="text"  placeholder=" affichage bdd">
+  <div class="col-sm-6 ">
+    <input type="radio" name="Configuration" value="yes" id="ConfigureYes"  required>
+    <label for="ConfigureYes">Oui</label>
+    <input type="radio" name="Configuration" value="no" id="ConfigureNo"   required>
+    <label for="ConfigureNo">Non</label>
   </div>
 </div>
+<!--
+data-display="block" data-divId="pMatos"
+data-display="none"  data-divId="pMatos"
+<div class="row" id="pMatos">
+<div class="col-sm-3" >
+<label>Prix Matériel : </label>
+</div>
+<div class="col-sm-6">
+<input type="text"  placeholder=" affichage bdd">
+</div>
+</div>-->
 <?php
 include ('sub_popup.php');
 
- ?>
+?>
